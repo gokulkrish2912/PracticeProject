bf1c127c8efe181fe5af878f30823606dba5e988
gokulkrish2912 <109265718+gokulkrish2912@users.noreply.github.com>
2022-08-27 14:46:56 +0530

    Handling User Authentication

---
+++ UserAuthentication/src/main/java/com/userauth/demo/AuthenticationUser.java
@@ -0,0 +1,16 @@
+package com.userauth.demo;
+
+public class AuthenticationUser {
+	public String username()
+	{
+		String email = "admin123@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "admin123";
+		return password;
+	}
+
+}
